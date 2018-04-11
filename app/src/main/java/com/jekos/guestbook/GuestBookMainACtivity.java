58b450062a8cb47ac625c2220c4d9bd99f940340
Eugene <golodnov@spark-mail.ru>
2018-04-11 18:23:49 +0300

    first commit

---
+++ app/src/main/java/com/jekos/guestbook/GuestBookMainACtivity.java
@@ -0,0 +1,13 @@
+package com.jekos.guestbook;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class GuestBookMainACtivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_guest_book_main_activity);
+    }
+}
