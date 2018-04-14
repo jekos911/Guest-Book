8a2aa3903c66e297581cb46e7d397b029e8b8673
Eugene <golodnov@spark-mail.ru>
2018-04-14 22:30:51 +0300

    ui

--- app/src/main/java/com/jekos/guestbook/GuestBookMainACtivity.java
+++ app/src/main/java/com/jekos/guestbook/GuestBookMainACtivity.java
@@ -1,12 +1,15 @@
 package com.jekos.guestbook;
 
+import android.app.Activity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import com.jekos.guestbook.db.DBHelper;
 import com.jekos.guestbook.models.Note;
@@ -37,9 +40,27 @@ public class GuestBookMainACtivity extends AppCompatActivity implements View.OnC
 
     }
 
+    void hideSoftKeyboard(Activity activity) {
+        InputMethodManager inputMethodManager = (InputMethodManager)  activity.getSystemService(Activity.INPUT_METHOD_SERVICE);
+        inputMethodManager.hideSoftInputFromWindow(activity.getCurrentFocus().getWindowToken(), 0);
+    }
+
     @Override
     public void onClick(View v) {
+        String fName = firstName.getText().toString();
+        String sName = lastName.getText().toString();
+
+        if (fName.isEmpty() | sName.isEmpty()) {
+            Toast.makeText(this,getString(R.string.notif_button),Toast.LENGTH_LONG).show();
+            return;
+        }
+
+        hideSoftKeyboard(this);
         Note note = dbHelper.insertRow(firstName.getText().toString(),lastName.getText().toString());
         notesAdapter.addNote(note);
+        firstName.setText(null);
+        firstName.requestFocus();
+        lastName.setText(null);
+
     }
 }
